2a4f052f30908e243d2be1d1584d0418a006db7d
adisb <bakytovadis@gmail.com>
2025-02-02 23:00:41 +0600

    Основная страница. Добавление стилей

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,19 @@
-<footer>
-    <p>© 2025 ОсОО "Нефтегазгеофизика". Все права защищены.</p>
+<footer class="py-4">
+    <div class="container">
+        <div class="row">
+            <!-- Левая часть (меню) -->
+            <div class="col-md-6 text-center text-md-start">
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-secondary text-decoration-none">О компании</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Услуги</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Контакты</a></li>
+                </ul>
+            </div>
+
+            <!-- Правая часть (название компании) -->
+            <div class="col-md-6 text-center text-md-end">
+                <p class="text-secondary">© 2025 ОсОО "Нефтегазгеофизика"</p>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,55 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Добро пожаловать!</h1>
-    <p>Это главная страница.</p>
+    <div class="main-content">
+        <div class="container">
+            <h1>Добро пожаловать на наш сайт!</h1>
+            <p>Мы рады представить вам нашу компанию, которая специализируется на геофизических исследованиях и нефтегазовых технологиях. Наша цель — предоставить высококачественные услуги в области нефтегазовой отрасли с использованием современных технологий и методов.</p>
+
+            <div class="row mb-4">
+                <!-- Карточка 1 -->
+                <div class="col-12">
+                    <div class="card flex-row">
+                        <!-- Изображение слева -->
+                        <img src="{{ asset('images/services.jpg') }}" class="card-img-left" alt="Our services">
+                        <div class="card-body">
+                            <h5 class="card-title">Наши услуги</h5>
+                            <p class="card-text">Познакомьтесь с нашими услугами в сфере геофизики и нефтегазовых исследований. Мы предлагаем передовые решения и опытных специалистов.</p>
+                            <a href="/services" class="btn btn-link">Перейти на страницу</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <!-- Карточка 4 -->
+                <div class="col-12">
+                    <div class="card flex-row">
+                        <!-- Изображение слева -->
+                        <img src="{{ asset('images/projects.jpg') }}" class="card-img-left" alt="Our projects">
+                        <div class="card-body">
+                            <h5 class="card-title">Наши проекты</h5>
+                            <p class="card-text">Изучите наши успешные проекты, которые помогли клиентам добиться значительных результатов в нефтегазовой и геофизической отрасли.</p>
+                            <a href="/projects" class="btn btn-link">Перейти на страницу</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <!-- Карточка 5 -->
+                <div class="col-12">
+                    <div class="card flex-row">
+                        <!-- Изображение слева -->
+                        <img src="{{ asset('images/team.jpg') }}" class="card-img-left" alt="Our team">
+                        <div class="card-body">
+                            <h5 class="card-title">Наша команда</h5>
+                            <p class="card-text">Наши специалисты обладают богатым опытом и высокими профессиональными навыками, что позволяет нам достигать отличных результатов.</p>
+                            <a href="/team" class="btn btn-link">Перейти на страницу</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
